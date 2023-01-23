7b0fe4ac0c643d30f600e5d2a786312ea865d927
unknown <carlo.miranda10@gmail.com>
2023-01-22 19:38:56 -0500

    initial commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,24 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+
+const dateToday = new Date().toLocaleString();
+const birthday = "HAPPY BIRTHDAY!!";
+const birthdate = ''
+
+function showDate (){
+  return dateToday;
+}
+
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Today's Date is {{showDate()}}</template>
+    <template #today></template>
+  </WelcomeItem>
+</template>
